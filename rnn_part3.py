aface2714d10e0f09a368631d13fe3043b10cb51
NataliaOviedoA <oviedoacostanatalia@gmail.com>
2022-12-02 18:03:59 +0100

    functional question3

--- rnn_part3.py
+++ rnn_part3.py
@@ -1,62 +1,13 @@
 import torch 
 from torch import nn
 import numpy as np
+import torch.nn.functional as F
 import load_dataset as data
+import torch.optim as optim
 
 (x_train, y_train), (x_val, y_val), (i2w, w2i), numcls = data.load_imdb(final=False)
 
-batch_size = 1000
-
-# Counters
-k = 0
-j = batch_size
-# Splitting the xtrain into batches.
-matrix = []  
-for i in range(20):
-    matrix.append(x_train[k:j])
-    k = j
-    j = j + batch_size
-
-maxLength = 0
-# Looping the matrix.
-for i in matrix:
-    # Getting the highest value from each value.
-    current_length = max(len(x) for x in i)
-    if current_length > maxLength:
-        maxLength = current_length
-
-# adding padding to each element of the list through a nested loop.
-for i in matrix:
-    for j in i:
-        while len(j) < maxLength:
-            j.append(0)
-
-hidden_size = 300
-learning_rate = 0.001
-input_size = 20000
-output_size = 20000
-
-# Size of the dictionary of embedding
-embedding_num = 99429
-# Embedding dimension - the size of each embedding vector
-embedding_dim = 300 
-# Embedding module
-embedding = nn.Embedding(embedding_num, embedding_dim)
-# Here - should be a loop over the 20 batches of the matrix
-# Only for matrix[0] as an example
-batch = torch.tensor(matrix[0], dtype=torch.long)
-# Batch with embedding 
-batch_embedding = embedding(batch)
-
-# Linear dimention for second layer after embedding
-hidden_dim = nn.Linear(embedding_dim, hidden_size)
-
-# Hidden layer
-hidden = hidden_dim(batch_embedding)
-
-# Relu activation for non-linearity
-hidden = torch.relu(hidden)
-
+# class for elman nn
 class Elman(nn.Module):
     def __init__(self, insize=300, outsize=300, hsize=300):
         super().__init__()
@@ -78,8 +29,79 @@ class Elman(nn.Module):
         
         return torch.cat(outs, dim=1), hidden
 
-model = Elman()
+#Class for the neural network
+class Net(nn.Module):
+    def __init__(self): 
+        super().__init__()
+        self.embedding = nn.Embedding(100000, 300)
+        self.fc1 = nn.Linear(300,300)
+        self.fc2 = nn.Linear(300,2)
+        self.hidden = None
+    def forward(self,x): #algorithm for the forward propagation
+        x = self.embedding(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x, hidden = ElmanNet.forward(x, self.hidden)
+        x,_ = torch.max(x,1)
+        x = self.fc2(x)
+        return x
+    
+def accuracy(x,y):
+    preds = torch.argmax(x,1)
+    return (torch.sum(preds == y)/len(y)).item()
 
-here = np.random.rand(1000,2154,300)
-here2 = torch.tensor(here, dtype=torch.long)
-out, hidden = model.forward(here2)
+
+#Setting hyperparameters
+num_epochs = 3
+learning_rate = 0.001
+batch_size = 30
+batch_iter = 500
+#Counters
+k = 0
+j = batch_size
+
+#Splitting the labels into batches
+labels = []
+k = 0
+j = batch_size
+for i in range(batch_iter):
+    labels.append(y_train[k:j])  
+    k = j
+    j = j+batch_size  
+
+
+#Padding xtrain
+xtrain = x_train[0:15000]
+max_length =len(xtrain[-1])
+for i in xtrain:
+    while len(i)<max_length:
+        i.append(0)
+
+#Data Inputs
+xtrain = torch.tensor(xtrain, dtype = torch.long)
+trainset = torch.utils.data.DataLoader(xtrain, batch_size)
+labels = torch.tensor(labels, dtype = torch.long)
+
+#Initialize network, loss and optimizer
+NeuralNet = Net()
+ElmanNet = Elman()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(NeuralNet.parameters(), lr=0.001)
+
+
+#Epochs and baches
+vector = []
+for epoch in range(num_epochs):
+    count = 0
+    
+    for i in trainset:
+        NeuralNet.zero_grad() #set gradients to zero
+        myNet = NeuralNet(i)#run forward
+        loss = criterion(myNet, labels[count])#calculating loss
+        print(accuracy(myNet,labels[count]))   
+        vector.append(accuracy(myNet,labels[count]))
+        count += 1
+        loss.backward() #backward process
+        optimizer.step() #iptimizer  
+    print('This is the epoch') 
+
