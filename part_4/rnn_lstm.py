fab67cf283f1f51285ed6a1a41ab93d7dd75a011
NataliaOviedoA <oviedoacostanatalia@gmail.com>
2022-12-10 10:59:20 +0100

    LSTM done

--- part_4/rnn_lstm.py
+++ part_4/rnn_lstm.py
@@ -1,13 +1,13 @@
 import torch 
 from torch import nn
-import numpy as np
 import torch.nn.functional as F
 import load_dataset as data
 import torch.optim as optim
 from torch.autograd import Variable 
 import matplotlib.pyplot as plt
 
-(x_train, y_train), (x_val, y_val), (i2w, w2i), numcls = data.load_imdb(final=True)
+(x_train, y_train), (x_val, y_val), (i2w, w2i), numcls = data.load_imdb(final=False)
+(x_train2, y_train2), (x_test, y_test), (i2w2, w2i2), numcls = data.load_imdb(final=True)
 
 #Class for the neural network
 class Net(nn.Module):
@@ -15,7 +15,7 @@ class Net(nn.Module):
         super().__init__()
         self.embedding = nn.Embedding(100000, 300)
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                          num_layers=n_layers, batch_first=True) #lstm
+                          num_layers=n_layers, batch_first=True, dropout=0.2) #lstm
         self.fc1 = nn.Linear(hidden_size, input_size)
         self.fc2 = nn.Linear(hidden_size,2)
         self.num_layers = n_layers
@@ -50,6 +50,9 @@ total = batch_size*batch_iter
 Tbatch_size = 50
 Tbatch_iter = 300
 Ttotal = Tbatch_size * Tbatch_iter
+Vbatch_size = 50
+Vbatch_iter = 100
+Vtotal = Vbatch_size * Vbatch_iter
 #Counters
 k = 0
 j = batch_size
@@ -63,12 +66,21 @@ for i in range(batch_iter):
     k = j
     j = j+batch_size    
 
-#splitting test_val labels into batches
+#splitting y_val labels into batches
+vallabels = []
+k = 0
+j = Vbatch_size
+for i in range(Vbatch_iter):
+    vallabels.append(y_val[k:j])  
+    k = j
+    j = j+Vbatch_size 
+
+#splitting y__test labels into batches
 testlabels = []
 k = 0
 j = Tbatch_size
 for i in range(Tbatch_iter):
-    testlabels.append(y_val[k:j])  
+    testlabels.append(y_test[k:j])  
     k = j
     j = j+Tbatch_size 
 
@@ -80,7 +92,14 @@ for i in xtrain:
         i.append(0)
 
 #Padding xvalue
-ttrain = x_val[0:Ttotal]
+vtrain = x_val[0:Vtotal]
+max_length =len(vtrain[-1])
+for i in vtrain:
+    while len(i)<max_length:
+        i.append(0)
+
+#Padding xtest
+ttrain = x_test[0:Ttotal]
 max_length =len(ttrain[-1])
 for i in ttrain:
     while len(i)<max_length:
@@ -92,19 +111,25 @@ trainset = torch.utils.data.DataLoader(xtrain, batch_size)
 labels = torch.tensor(labels, dtype = torch.long)
 
 #Data Inputs for validation
-x_valtrain = torch.tensor(ttrain, dtype = torch.long)
-validset = torch.utils.data.DataLoader(x_valtrain,Tbatch_size)
-validlabels = torch.tensor(testlabels, dtype = torch.long)
+x_valtrain = torch.tensor(vtrain, dtype = torch.long)
+valset = torch.utils.data.DataLoader(x_valtrain,Vbatch_size)
+vallabels = torch.tensor(vallabels, dtype = torch.long)
+
+#Data Inputs for test
+x_testtrain = torch.tensor(ttrain, dtype = torch.long)
+testset = torch.utils.data.DataLoader(x_testtrain,Tbatch_size)
+testlabels = torch.tensor(testlabels, dtype = torch.long)
 
 #Initialize network, loss and optimizer
 NeuralNet = Net(input_size, hidden_size, n_layers)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(NeuralNet.parameters(), lr=0.001)
+optimizer = optim.Adam(NeuralNet.parameters(), lr=learning_rate)
 
 
 #Epochs and baches
 vector = [0]
 valid_vector = [0]
+test_vector = [0]
 for epoch in range(num_epochs):
     count = 0
     acc = 0
@@ -124,22 +149,37 @@ for epoch in range(num_epochs):
     # vector.append(final_acc)
     vector.append(final_acc)
 
+    NeuralNet.eval()
+    count = 0
+    loss_mv = 0
+    acc2 = 0
+    for i in valset:
+        myNet = NeuralNet(i) 
+        loss = criterion(myNet, vallabels[count])
+        #loss_mv +=loss.item()
+        acc2 += accuracy(myNet,vallabels[count])
+        print("Acc = {0}".format(accuracy(myNet,vallabels[count])))
+        count += 1
+        
+    valid_vector.append(acc2/Vbatch_iter)
+
+#Testing
 count = 0
 loss_mv = 0
 acc2 = 0
-for i in validset:
+for i in testset:
     myNet = NeuralNet(i) 
-    loss = criterion(myNet, validlabels[count])
-    #loss_mv +=loss.item()
-    acc2 += accuracy(myNet,validlabels[count])
-    print("Acc = {0}".format(accuracy(myNet,validlabels[count])))
+    loss = criterion(myNet, testlabels[count])
+    acc2 += accuracy(myNet,testlabels[count])
+    print("Acc = {0}".format(accuracy(myNet,testlabels[count])))
     count += 1
     
-valid_vector.append(acc2/Tbatch_iter)
-print(valid_vector)
+test_vector.append(acc2/Tbatch_iter)
+print(test_vector)
 
 
 plt.plot(vector, label = "Training")
+plt.plot(valid_vector, color = 'r', label = "Validation")
 plt.title("Accuracy Training")
 plt.ylabel('Accuracy')
 plt.xlabel("Epochs")
